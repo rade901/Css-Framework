92cd0ef5c357ac69818a88d441b762e24e8b0e16
Rade Jasenovčanin <78330407+rade901@users.noreply.github.com>
2022-03-11 21:58:02 +0100

    nav commit

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', function() {
     this.classList.toggle('is-active');
 });
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
