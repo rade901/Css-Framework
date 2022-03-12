e4617d6e2c3287ea190bb6ff8a5e51b9c7e47a37
Rade Jasenovčanin <78330407+rade901@users.noreply.github.com>
2022-03-12 22:03:02 +0100

    dropdown

--- main.js
+++ main.js
@@ -11,3 +11,41 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+ /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myDrop() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myDrop1() {
+  document.getElementById("myDropdown1").classList.toggle("show1");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn1')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content1");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show1')) {
+        openDropdown.classList.remove('show1');
+      }
+    }
+  }
+}
